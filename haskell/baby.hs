ae4dfe1e78a9cd2a88185a2a87d7e8fe3ef08643
vanessa007 <licheng.hongkong@gmail.com>
2013-08-02 01:56:25 -0700

    delete

--- haskell/baby.hs
+++
@@ -1,5 +0,0 @@
-doubleMe x = x + x
-doubleUs x y = x * 2 + y * 2
-boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
-people = ["LILY", "LUCY", "ALICE"]
-describe = ["Beautiful", "Smart", "Amiable"]
