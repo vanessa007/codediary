b586da6e543e5917fa9f40e310350d71b1601177
vanessa007 <licheng.hongkong@gmail.com>
2013-08-03 03:36:25 -0700

    revised

--- haskell/runLenghtEncoding.hs
+++ haskell/runLenghtEncoding.hs
@@ -1,6 +1,7 @@
 runLengthEncoding :: (Eq a) => [a] -> [(a,Int)]
-runLengthEncoding (x:xs)
-    | xs == []  = [(x,1)]
+runLengthEncoding [] = []
+runLengthEncoding [x] = [(x,1)]
+runLengthEncoding (x:xs) 
     | x == a    = (x,b+1):(tail (runLengthEncoding xs))
     | otherwise = (x,1):(runLengthEncoding xs)
     where (a,b) = head (runLengthEncoding xs)
